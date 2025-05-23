21b728a563fc737c46d8e37399499cc4f239722f
valrun <valrun0@ya.ru>
2025-05-23 22:39:22 +0500

    Task1: added initialization Maven, Docker, Liquibase

---
+++ src/main/java/valrun/demo/txn_visualizer/TxnVisualizerApplication.java
@@ -0,0 +1,13 @@
+package valrun.demo.txn_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnVisualizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxnVisualizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/valrun/demo/txn_visualizer/TxnVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package valrun.demo.txn_visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnVisualizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
